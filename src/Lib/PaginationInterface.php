6c81a826de6a4a1cf8dec606efc95d1107c8ba7f
sharif-uddin1 <sharif.uddin1@vodafone.com>
2019-11-03 14:37:24 +0000

    fix

--- src/Lib/PaginationInterface.php
+++ src/Lib/PaginationInterface.php
@@ -4,13 +4,6 @@ namespace Pagination\Lib;
 
 interface PaginationInterface
 {
-    /**
-     * Paginate a collection of items
-     *
-     * @param $collection
-     * @param int $page
-     * @param int $limit
-     * @return PaginationInterface
-     */
+
     public function paginate(int $page = 1): PaginatorInterface;
 }
