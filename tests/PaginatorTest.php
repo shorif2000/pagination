7856ab6b709466c8b39120eb4175f692f5aef98e
sharif-uddin1 <sharif.uddin1@vodafone.com>
2019-11-05 10:56:17 +0000

    ArrayObjectPageProvider

--- tests/PaginatorTest.php
+++ tests/PaginatorTest.php
@@ -5,6 +5,7 @@ use PHPUnit\Framework\TestCase;
 use Pagination\Paginator;
 use Pagination\PaginatorFactory;
 use Pagination\Lib\ArrayPageProvider;
+use Pagination\Lib\ArrayObjectPageProvider;
 
 class PaginatorTest extends TestCase
 {
@@ -58,7 +59,7 @@ class PaginatorTest extends TestCase
         $input = array_slice($input, 0, 10);
         $input = new \ArrayObject($input);
         $pageNumber = 1;
-        $pagination = (new PaginatorFactory(new ArrayPageProvider($input)))->createPaginator($pageNumber, $itemsPerPage);
+        $pagination = (new PaginatorFactory(new ArrayObjectPageProvider($input)))->createPaginator($pageNumber, $itemsPerPage);
         
         $this->assertEquals($itemsPerPage, $pagination->getTotalOnCurrentPage());
         $this->assertEquals($pageNumber, $pagination->getCurrentPageNumber());
@@ -73,7 +74,7 @@ class PaginatorTest extends TestCase
         $input = (object) array_slice($input, 0, 20);
         $input = new \ArrayObject($input);
         $pageNumber = 2;
-        $pagination = (new PaginatorFactory(new ArrayPageProvider($input)))->createPaginator($pageNumber, $itemsPerPage);
+        $pagination = (new PaginatorFactory(new ArrayObjectPageProvider($input)))->createPaginator($pageNumber, $itemsPerPage);
         
         $this->assertEquals($itemsPerPage, $pagination->getTotalOnCurrentPage());
         $this->assertEquals($pageNumber, $pagination->getCurrentPageNumber());
@@ -87,7 +88,7 @@ class PaginatorTest extends TestCase
         $input = (object) array_slice($input, 0, 15);
         $input = new \ArrayObject($input);
         $pageNumber = 2;
-        $pagination = (new PaginatorFactory(new ArrayPageProvider($input)))->createPaginator($pageNumber, $itemsPerPage);
+        $pagination = (new PaginatorFactory(new ArrayObjectPageProvider($input)))->createPaginator($pageNumber, $itemsPerPage);
         
         $this->assertEquals(count(array_slice($input->getArrayCopy(), 10, 10)), $pagination->getTotalOnCurrentPage());
         $this->assertEquals($pageNumber, $pagination->getCurrentPageNumber());
