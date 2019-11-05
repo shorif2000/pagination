7da9d0224a469c6f6f8df7887e1ab9c198eb46bc
sharif-uddin1 <sharif.uddin1@vodafone.com>
2019-11-05 10:54:32 +0000

    add array object

---
+++ src/Lib/ArrayObjectPageProvider.php
@@ -0,0 +1,22 @@
+<?php 
+namespace Pagination\Lib;
+
+class ArrayObjectPageProvider implements PageProviderInterface
+{
+    private $items;
+    
+    public function __construct(\ArrayObject $items) {
+        
+        $this->items = $items;
+    }
+    
+    public function getTotalCount(): int
+    {
+        return \count($this->items);
+    }
+    
+    public function getPage(int $offset, int $limit): \Iterator
+    {
+        return new \ArrayIterator(\array_slice($this->items, $offset, $limit));
+    }
+}
